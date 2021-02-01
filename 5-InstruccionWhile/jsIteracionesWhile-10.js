561d8a250837f75197a1d1446f40fc6275b11133
Orking20 <mateoluchetti@hotmail.com>
2021-02-01 04:15:40 -0300

    Sin terminar

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -13,17 +13,16 @@ hasta que el usuario quiera, mostrar:
 function mostrar()
 {
 	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+	let respuesta;
+	let numeroIngresado;
+	let sumaNegativos = 0;
 
-	respuesta="si";
+	respuesta = "si";
 
-	while(respuesta=="si")
+	while(respuesta == "si")
 	{
-		
-		respuesta=prompt("desea continuar?");
+		respuesta = prompt("desea continuar?", "si");
 	}//fin del while
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+	document.write("la suma de negativos es: " + sumaNegativos);
 }//FIN DE LA FUNCIÓN
