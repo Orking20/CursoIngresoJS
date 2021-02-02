14dd2c5555fd533546e2f5643e8efb24cbb257d2
Orking20 <mateoluchetti@hotmail.com>
2021-02-02 15:42:42 -0300

    Hecho

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -2,18 +2,39 @@ function mostrar()
 {
 	let numero;
 	let cont;
+	let primo;
 
 	numero = prompt("Ingrese un número.");
 
-	if (numero > 1)
+	if (numero == 2)
 	{
-		for (cont = 0; cont <= numero; cont++)
+		alert("Es un número primo.")
+	}
+	else if (numero > 1)
+	{
+		for (cont = 2; cont < numero; cont++)
 		{
-			
+			if (numero % cont == 0)
+			{
+				primo = false;
+				break;
+			}
+			else
+			{
+				primo = true;
+			}
+		}
+		if (primo == true)
+		{
+			alert("Es un número primo.")
+		}
+		else
+		{
+			alert("No es un número primo.")
 		}
 	}
 	else
 	{
-		alert("El número no es primo.");
+		alert("No es un número primo.");
 	}
 }//FIN DE LA FUNCIÓN
