84cb42d72b277197e2f914b44d65c4edfd6c2316
Orking20 <mateoluchetti@hotmail.com>
2021-02-02 18:46:43 -0300

    Hecho

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,60 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	let precio1;
+	let precio2;
+	let precio3;
+	let suma;
+
+	precio1 = document.getElementById("txtIdPrecioUno").value;
+	precio2 = document.getElementById("txtIdPrecioDos").value;
+	precio3 = document.getElementById("txtIdPrecioTres").value;
+
+	precio1 = parseInt(precio1);
+	precio2 = parseInt(precio2);
+	precio3 = parseInt(precio3);
+
+	suma = precio1 + precio2 + precio3;
+	alert(suma);
 }
+
 function Promedio () 
 {
-	
+	let precio1;
+	let precio2;
+	let precio3;
+	let promedio;
+
+	precio1 = document.getElementById("txtIdPrecioUno").value;
+	precio2 = document.getElementById("txtIdPrecioDos").value;
+	precio3 = document.getElementById("txtIdPrecioTres").value;
+
+	precio1 = parseInt(precio1);
+	precio2 = parseInt(precio2);
+	precio3 = parseInt(precio3);
+
+	promedio = (precio1 + precio2 + precio3) / 3;
+	alert(promedio);
 }
+
 function PrecioFinal () 
 {
-	
+	let precio1;
+	let precio2;
+	let precio3;
+	let suma;
+	let iva;
+
+	precio1 = document.getElementById("txtIdPrecioUno").value;
+	precio2 = document.getElementById("txtIdPrecioDos").value;
+	precio3 = document.getElementById("txtIdPrecioTres").value;
+
+	precio1 = parseInt(precio1);
+	precio2 = parseInt(precio2);
+	precio3 = parseInt(precio3);
+
+	suma = (precio1 + precio2 + precio3)
+	iva = (suma * 21) / 100;
+
+	alert(suma + iva);
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
+	let precio;
+	let descuento;
+	let precioFinal;
 
+	precio = prompt("Ingrese el precio.");
+	descuento = prompt("Ingrese el descuento.");
+
+	descuento = (precio * descuento) / 100;
+	precioFinal = precio - descuento;
+
+	document.getElementById("elPrecioFinal").value = precioFinal;
 }
