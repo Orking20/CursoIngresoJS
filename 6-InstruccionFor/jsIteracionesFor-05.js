f0c5e382761ac8a3b991b58b20e5cfdcf6428831
Orking20 <mateoluchetti@hotmail.com>
2021-02-02 03:43:05 -0300

    Hecho

--- 6-InstruccionFor/jsIteracionesFor-05.js
+++ 6-InstruccionFor/jsIteracionesFor-05.js
@@ -1,7 +1,10 @@
 function mostrar()
 {
-	alert("ok");
+	let numero;
+	let contador;
 
-
-
+	for (contador = 0; contador != 9; contador = numero)
+	{
+		numero = prompt("Ingrese un número.");
+	}
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor-06.js
+++ 6-InstruccionFor/jsIteracionesFor-06.js
@@ -1,7 +1,12 @@
 function mostrar()
 {
-	alert("ok");
+	let contador;
+	let numero;
 
+	numero = prompt("Ingrese un número.");
 
-
+	for (contador = 2; contador <= numero; contador += 2)
+	{
+		alert(contador);
+	}
 }//FIN DE LA FUNCIÓN
