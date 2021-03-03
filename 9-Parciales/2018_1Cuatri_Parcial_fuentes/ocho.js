018abf6833933bd66c5564de09509738de11f4df
Orking20 <mateoluchetti@hotmail.com>
2021-03-03 03:35:09 -0300

    Hecho

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,84 @@
 function mostrar()
 {
+	var letra;
+	var numero;
+	var pares = 0;
+	var impares = 0;
+	var cantCeros = 0;
+	var sumaPositivos = 0;
+	var cantNumerosPositivos = 0;
+	var promedio;
+	var sumaNegativos = 0;
+	var numMaximo;
+	var numMinimo;
+	var letraMaxima;
+	var letraMinima;
+	var contador = 1;
+	var continuar;
 
+	do
+	{
+		do
+		{
+			letra = prompt("Ingrese una letra: ");
+		}while(!isNaN(letra));
+		do
+		{
+			numero = parseInt(prompt("Ingrese un número (entre -100 y 100): "));
+		}while(isNaN(numero) || numero < -100 || numero > 100);
+
+		if(contador == 1)
+		{
+			numMaximo = numero;
+			numMinimo = numero;
+
+			contador--;
+		}
+
+		if(numero % 2 == 0)
+		{
+			pares++;
+		}
+		else if(numero % 2 != 0)
+		{
+			impares++;
+		}
+
+		if(numero == 0)
+		{
+			cantCeros++;
+		}
+		else if(numero > 0)
+		{
+			sumaPositivos += numero;
+			cantNumerosPositivos++;
+		}
+		else if(numero < 0)
+		{
+			sumaNegativos += numero;
+		}
+
+		if(numero > numMaximo)
+		{
+			numMaximo = numero;
+			letraMaxima = letra;
+		}
+		else if(numero < numMinimo)
+		{
+			numMinimo = numero;
+			letraMinima = letra;
+		}
+
+		continuar = confirm("¿Desea continuar?");
+	}while(continuar == true);
+
+	promedio = sumaPositivos / cantNumerosPositivos;
+
+	document.write("Cantidad de números pares: " + pares + "<br>");
+	document.write("Cantidad de números impares: " + impares + "<br>");
+	document.write("Cantidad de ceros: " + cantCeros + "<br>");
+	document.write("Promedio de números positivos: " + promedio + "<br>");
+	document.write("Suma de números negativos: " + sumaNegativos + "<br>");
+	document.write("Número y letra máxima: " + numMaximo + " " + letraMaxima + "<br>");
+	document.write("Número y letra mínima: " + numMinimo + " " + letraMinima + "<br>");
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,52 @@
 function mostrar()
 {
+	var nota;
+	var sexo;
+	var sumaNotas = 0;
+	var promedio;
+	var contador = 1;
+	var notaMinima;
+	var sexoNotaMinima;
+	var cantVarones = 0;
 
+	for(var i = 0; i < 5; i++)
+	{
+		do
+		{
+			nota = parseInt(prompt("Ingrese una nota (del 0 al 10): "));
+		}while(isNaN(nota) || nota < 0 || nota > 10);
+		do
+		{
+			sexo = prompt("Ingrese el sexo (f / m): ");
+		}while(sexo != "f" && sexo != "m");
+
+		if(contador == 1)
+		{
+			notaMinima = nota;
+
+			contador--;
+		}
+
+		if(nota <= notaMinima)
+		{
+			notaMinima = nota;
+			sexoNotaMinima = sexo;
+		}
+
+		if(sexo == "m")
+		{
+			if(nota >= 6)
+			{
+				cantVarones++;
+			}
+		}
+
+		sumaNotas += nota;
+	}
+
+	promedio = sumaNotas / 5;
+
+	alert("Promedio de las notas totales: " + promedio);
+	alert("La nota más baja es " + notaMinima + " del sexo " + sexoNotaMinima);
+	alert("Cantidad de varones con nota mayor o igual a 6: " + cantVarones);
 }
